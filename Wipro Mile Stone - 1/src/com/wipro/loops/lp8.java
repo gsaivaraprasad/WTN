ca6eb4beae34a77bb6a8f4f2b7577516ed5f15f0
Sai Vara Prasad <Sai Vara Prasad@Macbook>
2020-06-12 15:26:50 +0530

    Program for Colour Validation

---
+++ Wipro Mile Stone - 1/src/com/wipro/loops/lp8.java
@@ -0,0 +1,23 @@
+package com.wipro.loops;
+public class lp8 {
+	public static void main(String args[])
+	{
+		String str=args[0];
+		char ch=str.charAt(0);
+		switch(ch) {
+		case 'R':
+			System.out.println("->Red");break;
+		case 'B':
+			System.out.println("->Blue");break;
+		case 'G':
+			System.out.println("->Orange");break;
+		case 'O':
+			System.out.println("->Yellow");break;
+		case 'Y':
+			System.out.println("->White");break;
+		default:
+			System.out.println("Invalid Code");break;
+		}
+			
+	}
+}
