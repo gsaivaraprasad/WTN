4e8d81aadf979b9732c65a02b587b47d4ed9a3a9
Sai Vara Prasad <Sai Vara Prasad@Macbook>
2020-06-12 12:30:53 +0530

    Program to check if a given integer number is Positive,Negative or Zero

---
+++ Wipro Mile Stone - 1/src/com/wipro/loops/lp1.java
@@ -0,0 +1,12 @@
+package com.wipro.loops;
+public class lp1 {
+public static void main(String[] args) {
+	int a = Integer.parseInt(args[0]);
+	if (a>0)
+		System.out.println("Positive");
+	else if (a<0)
+	    System.out.println("Negative");
+	else
+		System.out.println("Zero");		
+}
+}
