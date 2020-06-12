e7a59dd1d0d945531270313320f2ed4df634a008
Sai Vara Prasad <Sai Vara Prasad@Macbook>
2020-06-12 10:43:25 +0530

    Command Line Argument Problem - 3

---
+++ Wipro Mile Stone - 1/src/com/wipro/languagebasics/clap3.java
@@ -0,0 +1,8 @@
+package com.wipro.languagebasics;
+public class clap3 {
+public static void main(String[] args) {
+int a = Integer.parseInt(args[0]);
+int b = Integer.parseInt(args[1]);
+System.out.println("The Sum of "+ args[0]+ " and "+ args[1]+" is "+(a+b));
+}
+}
