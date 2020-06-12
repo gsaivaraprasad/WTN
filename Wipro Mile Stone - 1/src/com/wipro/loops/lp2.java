0f0ed692f16c738c1ac61d5b1f07585aead593dc
Sai Vara Prasad <Sai Vara Prasad@Macbook>
2020-06-12 12:47:38 +0530

    Program to check if a given integer number is odd or even.

---
+++ Wipro Mile Stone - 1/src/com/wipro/loops/lp2.java
@@ -0,0 +1,10 @@
+package com.wipro.loops;
+public class lp2 {
+public static void main(String[] args) {
+	int a = Integer.parseInt(args[0]);
+	if (a/2==0)
+		System.out.println("Even");
+	else 
+	    System.out.println("Odd");	
+}
+}
