bce9c611d9a6bdebbe429af8d68b7a5159b310a7
Sai Vara Prasad <Sai Vara Prasad@Macbook>
2020-06-15 10:19:02 +0530

    Flow Control Statement Problems

---
+++ Wipro Mile Stone - 1/src/com/wipro/flowcontrolstatements/lp3.java
@@ -0,0 +1,10 @@
+package com.wipro.flowcontrolstatements;
+public class lp3 {
+public static void main(String[] args) {
+String str = args[0];
+if(str.length() == 0)
+System.out.println("No Argument");
+else
+System.out.println("The Length of String and Entered String is "+str.length()+","+str);
+}
+}
