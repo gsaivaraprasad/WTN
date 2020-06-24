71c161951a978b68ecce346a03a309c130e31413
Sai Vara Prasad <Sai Vara Prasad@Macbook>
2020-06-24 19:24:17 +0530

    Array Problems

---
+++ Wipro Mile Stone - 1/src/com/wipro/arrays/ap2.java
@@ -0,0 +1,24 @@
+package com.wipro.arrays;
+public class ap2 {
+	public static void main(String args[])
+	{
+		int arr[]=new int[args.length];
+		for(int i=0;i<args.length;i++) {
+			arr[i]=Integer.parseInt(args[i]);
+		}
+		int max=arr[0],min=arr[0];
+		for (int i=0;i<arr.length;i++)
+		{
+			if(arr[i]>=max)
+			{
+				max=arr[i];
+			}
+			if(arr[i]<=min)
+			{
+				min=arr[i];
+			}
+		}System.out.println("min="+min+"  max="+max);
+		
+	}
+
+}
